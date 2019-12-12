f75bb07e0c9bbb8027a568e5b87e01ed1e5dbc17
JustCaptcha <higashi166@gmail.com>
2019-12-12 20:16:47 +0100

    Deploy gh-pages

---
+++ docs/precache-manifest.94b68d38062216e4d0566d9eb733023a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "430b7ae5427b09374c0ef8979400b005",
+    "url": "/SimpleJsQuiz/index.html"
+  },
+  {
+    "revision": "a6024e98fffee06fc1e1",
+    "url": "/SimpleJsQuiz/main.bundle.css"
+  },
+  {
+    "revision": "a6024e98fffee06fc1e1",
+    "url": "/SimpleJsQuiz/static/js/main.a6024e98.js"
+  },
+  {
+    "revision": "09619fe319f1e7cfb792be05c067b007",
+    "url": "/SimpleJsQuiz/static/js/main.a6024e98.js.LICENSE"
+  },
+  {
+    "revision": "dd60b11e9762b6e25f6d4ce981a1eeae",
+    "url": "/SimpleJsQuiz/static/media/Unofficial_JavaScript_logo_2.dd60b11e.svg"
+  }
+]);
