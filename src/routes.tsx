955977bcb2105472788790401dbc94bb19c572de
JustCaptcha <higashi166@gmail.com>
2019-12-12 20:31:20 +0100

    Fixes route's path

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,7 +3,7 @@ import { mount, route } from 'navi';
 import { HomePage } from './pages/Home/HomePage';
 
 export const routes = mount({
-    '/SimpleJsQuiz': route({
+    '/': route({
         title: 'Home page',
         view: <HomePage />
     }),
